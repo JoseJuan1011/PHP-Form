07f724e96ca5699f8f99a28fa99b3b98eb841dc5
VegasBeats <j.j.13sg10@gmail.com>
2021-10-09 00:20:25 +0100

    activaSQL bug detected

--- InsertarAction.php
+++ InsertarAction.php
@@ -1,3 +1,28 @@
 <?php
+    include "Connectiondb.php";
 
+    $activa=0;
+    if (!empty(isset($_POST['activa']))) {
+        $activa = 1;
+    }
+    else {
+        $activa = 0;
+    }
+
+    echo "activa = " . $activa;
+    echo gettype($activa);
+
+    $datos = array($_POST['codHotel'], $_POST['numhabitacion'], $_POST['capacidad'], $_POST['preciodia'], $activa);
+    $pdo = Connect_db();
+    
+    $stmt = $pdo -> prepare('insert into habitaciones values (?,?,?,?,?)');
+    $stmt->bindParam(1, $datos[0]);
+    $stmt->bindParam(2, $datos[1]);
+    $stmt->bindParam(3, $datos[2]);
+    $stmt->bindParam(4, $datos[3]);
+    $stmt->bindParam(5, $datos[4]);
+    $stmt->execute();
+
+    echo "Inserction Completed";
+    //header('Refresh: 5; URL=MainPage.php');
 ?>
